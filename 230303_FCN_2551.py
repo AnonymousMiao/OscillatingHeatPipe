d3eab734feba72b205323792695bd251e6a6e84e
Xuan Miao <31434065+AnonymousMiao@users.noreply.github.com>
2023-03-05 15:08:17 +0000

    modified:   230303_FCN_2551.py

--- 230303_FCN_2551.py
+++ 230303_FCN_2551.py
@@ -45,101 +45,101 @@ class MyDataset(Dataset):
 
     def __len__(self):
         return len(self.x)
-def main():
-    # Read the data from the Excel file
-    data = pd.read_excel('data_combine.xlsx')
 
-    dataset_1 = data                                                       # ALL
-    dataset_2 = data[(data.iloc[:, 0] >= 15) & (data.iloc[:, 0] <= 85)]    # 15-85
-    dataset_3 = data[(data.iloc[:, 0] < 15) |  (data.iloc[:, 0] > 85)]     # 0-15 + 85-100
+# Read the data from the Excel file
+data = pd.read_excel('data_combine.xlsx')
 
-    dataset=dataset_2.sample(frac=1).reset_index(drop=True)
-    # we can get absolutely error 0.30 error relative 11.0% with dataset_1
-    # we can get absolutely error 0.06 error relative 10.5% with dataset_2
-    # we can get absolutely error 1.40 error relative 18.9% with dataset_3  
+dataset_1 = data                                                       # ALL
+dataset_2 = data[(data.iloc[:, 0] >= 15) & (data.iloc[:, 0] <= 85)]    # 15-85
+dataset_3 = data[(data.iloc[:, 0] < 15) |  (data.iloc[:, 0] > 85)]     # 0-15 + 85-100
 
-    x = dataset.iloc[:, :2]  # Input features
-    y = dataset.iloc[:, 2]   # Output variable
+dataset=dataset_2.sample(frac=1).reset_index(drop=True)
+# we can get absolutely error 0.30 error relative 11.0% with dataset_1
+# we can get absolutely error 0.06 error relative 10.5% with dataset_2
+# we can get absolutely error 1.40 error relative 18.9% with dataset_3  
 
-    # Split the data into training and validation sets
+x = dataset.iloc[:, :2]  # Input features
+y = dataset.iloc[:, 2]   # Output variable
 
-    val_size = 11
-    train_size = len(dataset)-val_size 
-    train_x, val_x, train_y, val_y = train_test_split(dataset.iloc[:, :2], dataset.iloc[:, 2], test_size=val_size)
+# Split the data into training and validation sets
 
-    # Create the dataset and data loader
-    train_dataset = MyDataset(train_x, train_y)
-    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-    val_dataset = MyDataset(val_x, val_y)
-    val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
+val_size = 11
+train_size = len(dataset)-val_size 
+train_x, val_x, train_y, val_y = train_test_split(dataset.iloc[:, :2], dataset.iloc[:, 2], test_size=val_size)
 
-    # Create the neural network and the optimizer
-    net = Net_1()
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.001, weight_decay=0.001)
+# Create the dataset and data loader
+train_dataset = MyDataset(train_x, train_y)
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+val_dataset = MyDataset(val_x, val_y)
+val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
 
-    # Initialize the plot
-    plt.ion()
-    fig, ax = plt.subplots()
+# Create the neural network and the optimizer
+net = Net_1()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001, weight_decay=0.001)
 
-    train_loss_history = []
-    val_loss_history = []
-    epoch_history = []
+# Initialize the plot
+plt.ion()
+fig, ax = plt.subplots()
 
-    # Train the neural network
-    for epoch in range(1000):
-        net.train()
-        for batch_x, batch_y in train_loader:
-            optimizer.zero_grad()
-            output = net(batch_x)
-            loss = torch.nn.functional.mse_loss(output.squeeze(), batch_y)
-            
-            # Add L2 regularization to the loss function
-            l2_reg = 0
-            for param in net.parameters():
-                l2_reg += torch.norm(param)
-            loss += 0.02 * l2_reg
-            
-            loss.backward()
-            optimizer.step()
+train_loss_history = []
+val_loss_history = []
+epoch_history = []
 
-            train_loss_history.append(loss.item())
-            epoch_history.append(epoch + len(train_loss_history) / len(train_dataset))
+# Train the neural network
+for epoch in range(1000):
+    net.train()
+    for batch_x, batch_y in train_loader:
+        optimizer.zero_grad()
+        output = net(batch_x)
+        loss = torch.nn.functional.mse_loss(output.squeeze(), batch_y)
+        
+        # Add L2 regularization to the loss function
+        l2_reg = 0
+        for param in net.parameters():
+            l2_reg += torch.norm(param)
+        loss += 0.02 * l2_reg
+        
+        loss.backward()
+        optimizer.step()
 
-        net.eval()
-        val_loss = 0
-        with torch.no_grad():
-            for batch_x, batch_y in val_loader:
-                output = net(batch_x)
-                val_loss += torch.nn.functional.mse_loss(output.squeeze(), batch_y)
-            val_loss /= len(val_loader)
-
-            val_loss_history.append(val_loss.item())
-            
-        epochs_perPlot=200
-        if epoch % epochs_perPlot == (epochs_perPlot-1) :
-            # Update the plot with the latest loss values
-            ax.clear()
-            ax.plot(epoch_history, train_loss_history, label='Train Loss')
-            ax.plot(range(0, len(val_loss_history)), val_loss_history, label='Val Loss')
-            ax.set_xlabel('Epoch')
-            ax.set_ylabel('Loss')
-            ax.legend()
-            plt.draw()
-
-        epochs_perPrint=20
-        if epoch % epochs_perPrint == (epochs_perPrint-1) :
-            print(f"Epoch {epoch+1}, Train Loss: {loss.item():.4f}, Val Loss: {val_loss.item():.4f}")
+        train_loss_history.append(loss.item())
+        epoch_history.append(epoch + len(train_loss_history) / len(train_dataset))
 
     net.eval()
+    val_loss = 0
     with torch.no_grad():
-        output = net(torch.tensor(x.values, dtype=torch.float32))
-        error_1 = torch.abs(output.squeeze() - torch.tensor(y.values, dtype=torch.float32)).mean() / torch.tensor(y.values, dtype=torch.float32).mean()
-        error_2 = torch.abs((output.squeeze() - torch.tensor(y.values, dtype=torch.float32))/torch.tensor(y.values, dtype=torch.float32)).mean()
-        
-        print(f'Relative Error 1: {error_1:.2%}')
-        print(f'Relative Error 2: {error_2:.2%}')
+        for batch_x, batch_y in val_loader:
+            output = net(batch_x)
+            val_loss += torch.nn.functional.mse_loss(output.squeeze(), batch_y)
+        val_loss /= len(val_loader)
 
-    plt.show()
-    plt.ioff()
-    print('Done')
+        val_loss_history.append(val_loss.item())
+        
+    epochs_perPlot=200
+    if epoch % epochs_perPlot == (epochs_perPlot-1) :
+        # Update the plot with the latest loss values
+        ax.clear()
+        ax.plot(epoch_history, train_loss_history, label='Train Loss')
+        ax.plot(range(0, len(val_loss_history)), val_loss_history, label='Val Loss')
+        ax.set_xlabel('Epoch')
+        ax.set_ylabel('Loss')
+        ax.legend()
+        plt.draw()
+
+    epochs_perPrint=20
+    if epoch % epochs_perPrint == (epochs_perPrint-1) :
+        print(f"Epoch {epoch+1}, Train Loss: {loss.item():.4f}, Val Loss: {val_loss.item():.4f}")
+
+net.eval()
+with torch.no_grad():
+    output = net(torch.tensor(x.values, dtype=torch.float32))
+    error_1 = torch.abs(output.squeeze() - torch.tensor(y.values, dtype=torch.float32)).mean() / torch.tensor(y.values, dtype=torch.float32).mean()
+    error_2 = torch.abs((output.squeeze() - torch.tensor(y.values, dtype=torch.float32))/torch.tensor(y.values, dtype=torch.float32)).mean()
+    
+    print(f'Relative Error 1: {error_1:.2%}')
+    print(f'Relative Error 2: {error_2:.2%}')
+
+plt.show()
+plt.ioff()
+print('Done')
 
