a6743098b29649a8f2dbd1f5a38e92f31f735e33
Xuanbo Miao <xuanbomiao@gmail.com>
2023-07-28 15:35:07 -0500

    1

--- 230303_FCN_2551.py
+++ 230303_FCN_2551.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 # Define the neural network architecture
 
+
 class Net_1(torch.nn.Module):
     def __init__(self):
         super(Net_1, self).__init__()
@@ -18,6 +19,7 @@ class Net_1(torch.nn.Module):
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
 
 class Net_2(torch.nn.Module):
     def __init__(self):
@@ -35,6 +37,8 @@ class Net_2(torch.nn.Module):
         return x
 
 # Define the dataset class
+
+
 class MyDataset(Dataset):
     def __init__(self, x, y):
         self.x = torch.tensor(x.values, dtype=torch.float32)
@@ -46,17 +50,23 @@ class MyDataset(Dataset):
     def __len__(self):
         return len(self.x)
 
+
 # Read the data from the Excel file
 data = pd.read_excel('data_combine.xlsx')
 
 dataset_1 = data                                                       # ALL
 dataset_2 = data[(data.iloc[:, 0] >= 15) & (data.iloc[:, 0] <= 85)]    # 15-85
-dataset_3 = data[(data.iloc[:, 0] < 15) |  (data.iloc[:, 0] > 85)]     # 0-15 + 85-100
+dataset_3 = data[(data.iloc[:, 0] < 15) | (
+    data.iloc[:, 0] > 85)]     # 0-15 + 85-100
 
-dataset=dataset_2.sample(frac=1).reset_index(drop=True)
+dataset = dataset_2.sample(frac=1).reset_index(drop=True)
+dataset_val = dataset_3.sample(frac=1).reset_index(drop=True)
+
 # we can get absolutely error 0.30 error relative 11.0% with dataset_1
 # we can get absolutely error 0.06 error relative 10.5% with dataset_2
-# we can get absolutely error 1.40 error relative 18.9% with dataset_3  
+# we can get absolutely error 1.40 error relative 18.9% with dataset_3
+
+# Epoch 1000, Train Loss: 0.0464, Val Loss: 1.3381
 
 x = dataset.iloc[:, :2]  # Input features
 y = dataset.iloc[:, 2]   # Output variable
@@ -64,8 +74,11 @@ y = dataset.iloc[:, 2]   # Output variable
 # Split the data into training and validation sets
 
 val_size = 11
-train_size = len(dataset)-val_size 
-train_x, val_x, train_y, val_y = train_test_split(dataset.iloc[:, :2], dataset.iloc[:, 2], test_size=val_size)
+train_size = len(dataset)-val_size
+train_x, val_x, train_y, val_y = train_test_split(
+    dataset.iloc[:, :2], dataset.iloc[:, 2], test_size=val_size)
+train_x_, val_x, train_y_, val_y = train_test_split(
+    dataset_val.iloc[:, :2], dataset_val.iloc[:, 2], test_size=val_size)
 
 # Create the dataset and data loader
 train_dataset = MyDataset(train_x, train_y)
@@ -92,18 +105,19 @@ for epoch in range(1000):
         optimizer.zero_grad()
         output = net(batch_x)
         loss = torch.nn.functional.mse_loss(output.squeeze(), batch_y)
-        
+
         # Add L2 regularization to the loss function
         l2_reg = 0
         for param in net.parameters():
             l2_reg += torch.norm(param)
         loss += 0.02 * l2_reg
-        
+
         loss.backward()
         optimizer.step()
 
         train_loss_history.append(loss.item())
-        epoch_history.append(epoch + len(train_loss_history) / len(train_dataset))
+        epoch_history.append(
+            epoch + len(train_loss_history) / len(train_dataset))
 
     net.eval()
     val_loss = 0
@@ -114,32 +128,35 @@ for epoch in range(1000):
         val_loss /= len(val_loader)
 
         val_loss_history.append(val_loss.item())
-        
-    epochs_perPlot=200
-    if epoch % epochs_perPlot == (epochs_perPlot-1) :
+
+    epochs_perPlot = 200
+    if epoch % epochs_perPlot == (epochs_perPlot-1):
         # Update the plot with the latest loss values
         ax.clear()
         ax.plot(epoch_history, train_loss_history, label='Train Loss')
-        ax.plot(range(0, len(val_loss_history)), val_loss_history, label='Val Loss')
+        ax.plot(range(0, len(val_loss_history)),
+                val_loss_history, label='Val Loss')
         ax.set_xlabel('Epoch')
         ax.set_ylabel('Loss')
         ax.legend()
         plt.draw()
 
-    epochs_perPrint=20
-    if epoch % epochs_perPrint == (epochs_perPrint-1) :
-        print(f"Epoch {epoch+1}, Train Loss: {loss.item():.4f}, Val Loss: {val_loss.item():.4f}")
+    epochs_perPrint = 20
+    if epoch % epochs_perPrint == (epochs_perPrint-1):
+        print(
+            f"Epoch {epoch+1}, Train Loss: {loss.item():.4f}, Val Loss: {val_loss.item():.4f}")
 
 net.eval()
 with torch.no_grad():
     output = net(torch.tensor(x.values, dtype=torch.float32))
-    error_1 = torch.abs(output.squeeze() - torch.tensor(y.values, dtype=torch.float32)).mean() / torch.tensor(y.values, dtype=torch.float32).mean()
-    error_2 = torch.abs((output.squeeze() - torch.tensor(y.values, dtype=torch.float32))/torch.tensor(y.values, dtype=torch.float32)).mean()
-    
+    error_1 = torch.abs(output.squeeze() - torch.tensor(y.values, dtype=torch.float32)
+                        ).mean() / torch.tensor(y.values, dtype=torch.float32).mean()
+    error_2 = torch.abs((output.squeeze() - torch.tensor(y.values, dtype=torch.float32)
+                         )/torch.tensor(y.values, dtype=torch.float32)).mean()
+
     print(f'Relative Error 1: {error_1:.2%}')
     print(f'Relative Error 2: {error_2:.2%}')
 
 plt.show()
 plt.ioff()
 print('Done')
-
--- 230306_Clustering_3d.py
+++ 230306_Clustering_3d.py
@@ -20,9 +20,27 @@ k = 3
 kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
 kmeans_labels = kmeans.labels_
 
+n_kmeans_labels = np.ones(len(X[:, 0]))
+for i in range(len(kmeans_labels)):
+    if kmeans_labels[i] == 2:
+        n_kmeans_labels[i] = 1
+    if kmeans_labels[i] == 1:
+        n_kmeans_labels[i] = 2
+    if kmeans_labels[i] == 0:
+        n_kmeans_labels[i] = 0
+
 # Gaussian mixture model clustering
 gmm = GaussianMixture(n_components=k, random_state=0).fit(X)
 gmm_labels = gmm.predict(X)
+
+n_gmm_labels = np.ones(len(X[:, 0]))
+for i in range(len(gmm_labels)):
+    if gmm_labels[i] == 2:
+        n_gmm_labels[i] = 0
+    if gmm_labels[i] == 1:
+        n_gmm_labels[i] = 2
+    if gmm_labels[i] == 0:
+        n_gmm_labels[i] = 1
 
 # Density-based clustering
 dbscan = DBSCAN(eps=0.3, min_samples=5).fit(X)
@@ -31,6 +49,40 @@ dbscan_labels = dbscan.labels_
 # Hierarchical clustering
 hierarchical = AgglomerativeClustering(n_clusters=k).fit(X)
 hierarchical_labels = hierarchical.labels_
+
+n_hierarchical_labels = np.ones(len(X[:, 0]))
+for i in range(len(hierarchical_labels)):
+    if hierarchical_labels[i] == 0:
+        n_hierarchical_labels[i] = 0
+    if hierarchical_labels[i] == 1:
+        n_hierarchical_labels[i] = 2
+    if hierarchical_labels[i] == 2:
+        n_hierarchical_labels[i] = 1
+
+LL_manu = np.ones(len(X[:, 0]))
+
+for i in range(len(X[:, 0])):
+    if X[i, 0] < 0.15:
+        LL_manu[i] = 0
+    elif X[i, 0] > 0.85:
+        LL_manu[i] = 2
+
+# Convert X to a DataFrame
+# Replace 'Column1', 'Column2', 'Column3' with actual column names
+X_df = pd.DataFrame(X, columns=['Column1', 'Column2', 'Column3'])
+
+# Add the additional variables as new columns to X_df
+X_df['LL_manu'] = LL_manu
+X_df['KMeans'] = n_kmeans_labels
+X_df['GMM'] = n_gmm_labels
+X_df['DBSCAN'] = dbscan_labels
+X_df['Hierarchical'] = n_hierarchical_labels
+
+
+# Save X_df to an Excel file
+output_file_path = 'output_data.xlsx'
+# Save X_df to the Excel file without including the index
+X_df.to_excel(output_file_path, index=False)
 
 # Plot the data points with colors corresponding to their assigned labels
 fig = plt.figure(figsize=(12, 12))
